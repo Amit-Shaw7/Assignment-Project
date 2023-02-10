ed0c3e7a91157f1ad5ff0f698549b120bb552835
Amit-Shaw7 <maahiamit777@gmail.com>
2023-02-10 12:12:20 +0530

    db string fixed

--- Backend/db.js
+++ Backend/db.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 mongoose.set('strictQuery', true);
 export const connectToMongo = async () => {
+    // console.log(process.env.MONGO_URI);
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URI_LOCAL);
+        const connection = await mongoose.connect(process.env.MONGO_URI);
         if (connection) {
             console.log("Connected to database");
         }
